df1005f68ef85935cdf5c7c02d2b8582c09b93da
skybru <giorgio.bruschi98@gmail.com>
2024-07-27 12:36:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+    const inputs = [];
+    const quotients = [];
+    const remainders = [];
+    
+    while (input > 0) {
+        input = 0;
+        const quotient = Math.floor(input / 2);
+        input = quotient;
+        const remainder = input % 2;
+        inputs.push(input);
+        quotients.push(quotient);
+        remainders.push(remainder);
+        result.innerText = remainders.reverse().join("");
+        
+        console.log("Inputs: ", inputs);
+        console.log("Quotients: ", quotients);
+        console.log("Remainders: ", remainders);
+    }
+    return "1";
+}
+
+const checkUserInput = () => {
+    if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
+        alert("Please provide a decimal number greater than or equal to 0"); //windows.alert() oppure alert()
+        return;
+    }
+
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
+}
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+       checkUserInput();
+
+    }
+});
