4d21fcf379296fee5cba6dd797ea71f6a91406c1
skybru <giorgio.bruschi98@gmail.com>
2024-07-27 21:56:37 +0200

    added recursive function

--- script.js
+++ script.js
@@ -3,25 +3,11 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-    const inputs = [];
-    const quotients = [];
-    const remainders = [];
-    
-    while (input > 0) {
-        input = 0;
-        const quotient = Math.floor(input / 2);
-        input = quotient;
-        const remainder = input % 2;
-        inputs.push(input);
-        quotients.push(quotient);
-        remainders.push(remainder);
-        result.innerText = remainders.reverse().join("");
-        
-        console.log("Inputs: ", inputs);
-        console.log("Quotients: ", quotients);
-        console.log("Remainders: ", remainders);
-    }
-    return "1";
+    if (input === 0 || input === 1) {
+        return String(input);
+      } else {
+        return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+      }
 }
 
 const checkUserInput = () => {
@@ -30,8 +16,9 @@ const checkUserInput = () => {
         return;
     }
 
-    decimalToBinary(parseInt(numberInput.value));
+    result.textContent = decimalToBinary(parseInt(numberInput.value));
     numberInput.value = "";
+    
 }
 
 convertBtn.addEventListener("click", checkUserInput);
